c4c225ac77c1a29bed63bdacf5587101d1f62e36
Clay <83361004+Lunar-Orbit1@users.noreply.github.com>
2023-01-16 14:59:04 -0800

    Gone, reduced to atoms

--- setup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo If you have not installed Node.js, Noblox.js, or colors please see the readme file
-echo Please copy and paste your .ROBLOSECURITY code below. If you do not know how to get it, please read 'README.md'
-set /p "id=Enter TOKEN (Remove the warning text): "
-node ./JAVASCRIPT/setup.js %id%
-pauseimage.png
